3d0694ab2064fb556f66bea38fd3f460fe5846af
Deng <nelsondeng0701@gmail.com>
2025-01-08 17:53:31 +1300

    add function

--- exampleProject3/ECommerce/Model.cs
+++ exampleProject3/ECommerce/Model.cs
@@ -1,8 +1,38 @@
+// Models.cs
+using System;
 using System.Collections.Generic;
 
 namespace ECommerce.Models
 {
-    // 商品接口
+    public class ECommerceSystem
+    {
+        private readonly IInventoryManager _inventoryManager;
+
+        public ECommerceSystem(IInventoryManager inventoryManager)
+        {
+            _inventoryManager = inventoryManager;
+        }
+
+        public void SellProduct(IProduct product, int quantity, ISalesStrategy salesStrategy)
+        {
+            if (_inventoryManager.ReduceStock(product, quantity))
+            {
+                salesStrategy.Apply(product);
+                Console.WriteLine($"{quantity} units of {product.Name} sold at ${product.Price} each.");
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient stock for {product.Name}.");
+            }
+        }
+
+        public void AddStock(IProduct product, int quantity)
+        {
+            _inventoryManager.AddStock(product, quantity);
+            Console.WriteLine($"{quantity} units of {product.Name} added to inventory.");
+        }
+    }
+
     public interface IProduct
     {
         string Name { get; }
@@ -10,39 +40,40 @@ namespace ECommerce.Models
         void ApplyDiscount(decimal percentage);
     }
 
-    // 商品类：电子产品
     public class Electronic : IProduct
     {
         public string Name { get; private set; }
         public decimal Price { get; private set; }
+
         public Electronic(string name, decimal price)
         {
             Name = name;
             Price = price;
         }
+
         public void ApplyDiscount(decimal percentage)
         {
             Price -= Price * percentage / 100;
         }
     }
 
-    // 商品类：服装
     public class Clothing : IProduct
     {
         public string Name { get; private set; }
         public decimal Price { get; private set; }
+
         public Clothing(string name, decimal price)
         {
             Name = name;
             Price = price;
         }
+
         public void ApplyDiscount(decimal percentage)
         {
             Price -= Price * percentage / 100;
         }
     }
 
-    // 库存管理接口
     public interface IInventoryManager
     {
         void AddStock(IProduct product, int quantity);
@@ -50,7 +81,6 @@ namespace ECommerce.Models
         int GetStock(IProduct product);
     }
 
-    // 库存管理实现
     public class InventoryManager : IInventoryManager
     {
         private readonly Dictionary<IProduct, int> _inventory = new();
@@ -79,20 +109,20 @@ namespace ECommerce.Models
         }
     }
 
-    // 销售策略接口
     public interface ISalesStrategy
     {
         void Apply(IProduct product);
     }
 
-    // 折扣策略实现
     public class DiscountStrategy : ISalesStrategy
     {
         private readonly decimal _discountPercentage;
+
         public DiscountStrategy(decimal discountPercentage)
         {
             _discountPercentage = discountPercentage;
         }
+
         public void Apply(IProduct product)
         {
             product.ApplyDiscount(_discountPercentage);
--- exampleProject3/ECommerce/Program.cs
+++ exampleProject3/ECommerce/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Program.cs
+using System;
+using Microsoft.Extensions.DependencyInjection;
 using ECommerce.Models;
 
 namespace ECommerce
@@ -7,54 +9,32 @@ namespace ECommerce
     {
         public static void Main(string[] args)
         {
-            // 初始化库存管理和系统
-            IInventoryManager inventoryManager = new InventoryManager();
-            ECommerceSystem system = new ECommerceSystem(inventoryManager);
+            var serviceCollection = new ServiceCollection();
 
-            // 创建商品
+            
+            serviceCollection.AddSingleton<IInventoryManager, InventoryManager>();
+            serviceCollection.AddSingleton<ISalesStrategy>(provider => new DiscountStrategy(10)); // 10% 折扣
+            serviceCollection.AddTransient<ECommerceSystem>();
+
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+
+            var system = serviceProvider.GetRequiredService<ECommerceSystem>();
+
+
             IProduct laptop = new Electronic("Laptop", 1500);
             IProduct shirt = new Clothing("Shirt", 50);
-
-            // 添加库存
+            
             system.AddStock(laptop, 10);
             system.AddStock(shirt, 20);
 
-            // 定义销售策略
-            ISalesStrategy discount = new DiscountStrategy(10); // 10% 折扣
+           
+            var discountStrategy = serviceProvider.GetRequiredService<ISalesStrategy>();
 
-            // 执行销售
-            system.SellProduct(laptop, 2, discount);
-            system.SellProduct(shirt, 5, discount);
-        }
-    }
-
-    // 电子商务系统
-    public class ECommerceSystem
-    {
-        private readonly IInventoryManager _inventoryManager;
-
-        public ECommerceSystem(IInventoryManager inventoryManager)
-        {
-            _inventoryManager = inventoryManager;
-        }
-
-        public void SellProduct(IProduct product, int quantity, ISalesStrategy salesStrategy)
-        {
-            if (_inventoryManager.ReduceStock(product, quantity))
-            {
-                salesStrategy.Apply(product);
-                Console.WriteLine($"{quantity} units of {product.Name} sold at ${product.Price} each.");
-            }
-            else
-            {
-                Console.WriteLine($"Insufficient stock for {product.Name}.");
-            }
-        }
-
-        public void AddStock(IProduct product, int quantity)
-        {
-            _inventoryManager.AddStock(product, quantity);
-            Console.WriteLine($"{quantity} units of {product.Name} added to inventory.");
+ 
+            system.SellProduct(laptop, 2, discountStrategy);
+            system.SellProduct(shirt, 5, discountStrategy);
         }
     }
 }
